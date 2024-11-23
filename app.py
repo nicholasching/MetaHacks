d6080b975054c39fb7e0e052c0147278b81028f4
SolidBrick <terrenceyang06@gmail.com>
2024-11-23 16:44:11 -0500

    Make generate cards api endpoint

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request
 import os
 from audioTranscriber import audioTranscriber as at
 from summaryGenerator import summaryGenerator as sg
+from flashcardGenerator import flashcardGenerator as fg
 
 app = Flask(__name__)
 
@@ -13,7 +14,16 @@ def home():
 def to_back(test):
     file = open("text.txt", "w")
     file.write(at.transcribeAudio("sampleAudio.wav"))
+    file.close()
     return sg.generateSummary("text.txt")
+
+@app.route('/to_back/gen_cards', methods=['GET'])
+def generate_cards(path):
+    with open("text.txt", "r") as file:
+        line = file.readline() 
+        fg.generateCards(line)
+        return fg.getCards()
+        
 
 if __name__ == '__main__':
     app.run(debug=True)
