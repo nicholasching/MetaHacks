0d875b9d4d2151a6300396c71ca1aa64dbe9248d
SolidBrick <terrenceyang06@gmail.com>
2024-11-24 10:56:06 -0500

    Update JSON generation

--- flashcardGenerator.py
+++ flashcardGenerator.py
@@ -8,33 +8,40 @@ class flashcardGenerator() :
     self.cards = None
 
   def generateCards(self):
-    flashcardString: ChatResponse = chat(model='llama3.2', messages=[
-      {
-        'role': 'user',
-        'content': "based on the text: " + self.transcription + ', generate exactly 1 calculation based question and its corresponding answer. Return the result in the form of a JSON object with the following structure and without any extra space or text: \n'
-        + '{\n'
-        + '\t\"card1\": {\n'
-        + '\t\t\"q\": \"Question text here\",\n'
-        + '\t\t\"a\": \"Answer text here\"\n'
-        + '\t}\n'
-        + '}'
-      },
-    ])
-    msg = flashcardString.message.content
-    ind = msg.index('{') 
-    print(msg[ind:])
-    self.cards = json.loads(msg[ind:])
-    print(self.cards)
+    while True:
+        try:
+            flashcardString: ChatResponse = chat(model='llama3.2', messages=[
+            {
+              'role': 'user',
+              'content': "based on the text: " + self.transcription + ', generate exactly 1 calculation based question and its corresponding answer. Return the result in the form of a JSON object with the following structure and without any extra space or text: \n'
+              + '{\n'
+              + '\t\"card1\": {\n'
+              + '\t\t\"q\": \"Question text here\",\n'
+              + '\t\t\"a\": \"Answer text here\"\n'
+              + '\t}\n'
+              + '}'
+            }])
+            msg = flashcardString.message.content
+            ind = msg.index('{') 
+            print(msg[ind:])
+            self.cards = json.loads(msg[ind:])
+            print()
+            print(self.cards)
+            break
+        except: 
+            print("improper syntax, regenerating")
+    
     
   def getCards(self): 
     if self.cards == None: raise Exception ("No cards were generated")
     else: return self.cards
 
 def main():
-  gen = flashcardGenerator('Cats are cool')
+  gen = flashcardGenerator('basic addition')
   gen.generateCards()
   cardsJSON = gen.getCards()
-  print(type(cardsJSON))
+  print()
   print(cardsJSON["card1"])
+  print("done")
 
 main()
